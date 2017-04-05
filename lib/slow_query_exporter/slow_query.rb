e3bc84674858c4725fb1ed86fec66702ccdffd30
Remi Broemeling <remi.broemeling@clio.com>
2017-04-05 07:35:31 -0600

    Fix parsing of 'EXPLAIN' and 'REPLACE' queries
    'EXPLAIN' queries and 'REPLACE INTO' queries were not properly recognized by the
    Slow Query Exporter. This simple change updates the query definition so that
    they will be recognized and parsed correctly.

--- lib/slow_query_exporter/slow_query.rb
+++ lib/slow_query_exporter/slow_query.rb
@@ -25,7 +25,7 @@ module SlowQueryExporter
       innowait: /^#\s+InnoDB_rec_lock_wait: (\S+)\s+InnoDB_queue_wait: (\S+)/,
       innopage: /^#\s+InnoDB_pages_distinct: (\d+)/,
       time:     /^SET timestamp=(\d+)/,
-      query:    /^(SELECT|INSERT|UPDATE|DELETE|SHOW)\b/,
+      query:    /^(DELETE|EXPLAIN|INSERT|REPLACE|SELECT|SHOW|UPDATE)\b/,
     }
 
     attr_accessor :attributes
